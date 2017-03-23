a9d118948ecfad9e856e16fb2d582bd5830011aa
James Benner <james_benner@abercrombie.com>
2017-03-23 18:26:37 -0400

    Added "initializes with a blank paper" spec.

---
+++ test/PencilDurabilityKata.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const expect = require('chai').expect;
+const PencilDurabilityKata = require('../lib/PencilDurabilityKata');
+
+describe('PencilDurabilityKata', function () {
+    beforeEach(function () {
+        this.kata = new PencilDurabilityKata;
+    });
+
+    describe('Write', function () {
+        it('initializes with a blank paper', function () {
+            expect(this.kata.paper).to.equal('');
+        });
+    });
+});
