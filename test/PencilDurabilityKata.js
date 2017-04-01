bf5f6fde3a59f0a2c777364c9e9e0aa4784e9a6f
James Benner <james_benner@abercrombie.com>
2017-04-01 14:14:44 -0400

    Refactored `PencilDurabilityKata` class into `Pencil`.

--- test/PencilDurabilityKata.js
+++ test/PencilDurabilityKata.js
@@ -2,29 +2,23 @@
 
 const expect = require('chai').expect;
 const Pencil = require('../lib/Pencil');
-const PencilDurabilityKata = require('../lib/PencilDurabilityKata');
 
 describe('PencilDurabilityKata', function () {
-    beforeEach(function () {
-        this.kata = new PencilDurabilityKata;
-    });
-
     describe('Write', function () {
-        it('initializes with a blank paper', function () {
-            expect(this.kata.paper).to.equal('');
-        });
-
         it('writes to paper', function () {
             let text = 'Hello world!';
 
-            this.kata.write(text);
-            expect(this.kata.paper).to.equal(text);
+            expect(new Pencil(500).write(text)).to.equal(text);
         });
 
         it('should append text to the paper', function () {
-            this.kata.write('She sells sea shells');
-            this.kata.write(' down by the sea shore');
-            expect(this.kata.paper).to.equal('She sells sea shells down by the sea shore');
+            let pencil = new Pencil(500);
+            let paper = '';
+
+            paper = pencil.write('She sells sea shells', paper);
+            paper = pencil.write(' down by the sea shore', paper);
+
+            expect(paper).to.equal('She sells sea shells down by the sea shore');
         });
     });
 
