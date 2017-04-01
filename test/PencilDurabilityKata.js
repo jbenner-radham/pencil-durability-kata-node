086e4005019568cc568f57d86bf79b2ed043f94c
James Benner <james_benner@abercrombie.com>
2017-04-01 13:18:22 -0400

    Added "initializes a pencil with a point durability" spec.

--- test/PencilDurabilityKata.js
+++ test/PencilDurabilityKata.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const expect = require('chai').expect;
+const Pencil = require('../lib/Pencil');
 const PencilDurabilityKata = require('../lib/PencilDurabilityKata');
 
 describe('PencilDurabilityKata', function () {
@@ -24,6 +25,15 @@ describe('PencilDurabilityKata', function () {
             this.kata.write('She sells sea shells');
             this.kata.write(' down by the sea shore');
             expect(this.kata.paper).to.equal('She sells sea shells down by the sea shore');
+        });
+    });
+
+    describe('Point Degradation', function () {
+        it('initializes a pencil with a point durability', function () {
+            let pointDurability = 9001;
+            let pencil = new Pencil(pointDurability);
+
+            expect(pencil.pointDurability).to.equal(pointDurability);
         });
     });
 });
